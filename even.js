14c115c3bf0466c756f19c4fd24c382f1755a7d3
Irina G <irina.golionko@gmail.com>
2023-02-28 23:00:23 -0500

    added function to check if a number is even

---
+++ even.js
@@ -0,0 +1,7 @@
+//isEven takes as its parameter a number and returns a Boolean value representing whether or not the number is even.
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+
+console.log(isEven(5));
+console.log(isEven(12));
