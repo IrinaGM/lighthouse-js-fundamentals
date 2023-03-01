2c5d75aa8a58438039fb7dc2386824338ada323b
Irina G <irina.golionko@gmail.com>
2023-02-28 22:48:56 -0500

    Added example for declaration and functions calling

---
+++ hello.js
@@ -0,0 +1,11 @@
+//Declaring and calling functions examples
+
+//example #1
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHello("Irina");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
